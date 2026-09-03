52f80862ec146a7157efb2176923f23bc0022b69
Ashwin K Aggarwal <aka96@cornell.edu>
2026-09-03 12:27:39 -0400

    Solve transformers-attention

--- transformer/transformers-attention/transformers-attention.py
+++ transformer/transformers-attention/transformers-attention.py
@@ -11,15 +11,15 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     *batch, n_q, d_q = Q.shape
     *batch, n_k, d_k = K.shape
     *batch, n_v, d_v = V.shape
+    #the last two dimensions give sequence_length and dimensionality
+    #the rest of it is just feature information which is getting multiplied
 
     scores = Q @ K.transpose(-2,-1) 
     #switching last two axes, dont want to switch batch dimensions
     scaled_score = scores / math.sqrt(d_k)
     
     W = F.softmax(scaled_score, dim = -1)
-    # weights is size n_q x n_k
-    #all the ns should be equal
-    #V is size 
+    # last dimnesion gives the weights for embeddings
 
     output = W @ V
     return output
